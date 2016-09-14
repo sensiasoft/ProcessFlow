4451ccad5e984f1b3e97c4107cc7094d510bba0a
Brett McWilliams <brett-iMac@Bretts-iMac.local>
2016-09-13 23:38:12 -0500

    Major update to v2.0

--- prototypes.js
+++ prototypes.js
@@ -5,21 +5,6 @@ String.prototype.isUpper = function () {
     if (char == char.toUpperCase()) rtn = true;
     return rtn;
 }
-String.prototype.width = function (font) {
-    var f = font || '12px arial',
-        o = $('<div>' + this + '</div>')
-        .css({
-            'position': 'absolute',
-            'float': 'left',
-            'white-space': 'nowrap',
-            'visibility': 'hidden',
-            'font': f
-        })
-        .appendTo($('body')),
-        w = o.width();
-    o.remove();
-    return w + 100;
-}
 String.prototype.width = function(font) {
   var f = font || '12px arial',
       o = $('<div>' + this + '</div>')
